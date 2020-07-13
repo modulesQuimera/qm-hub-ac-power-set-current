71995dd806d2c34e6e295725bd82db96f3d4cdf6
gabisteph <gabriellepiresmestrinho@gmail.com>
2020-07-13 17:44:46 -0400

    update set current version 1.0.3

--- set_current.js
+++ set_current.js
@@ -2,26 +2,28 @@ module.exports = function(RED) {
 
     function SetCurrentNode(config) {
         RED.nodes.createNode(this, config);
-        var node = this
         this.current_value = config.current_value
+        this.slot = config.slot
+        var node = this
 
         node.on('input', function(msg, send, done) {
             var globalContext = node.context().global;
             var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
             var command = {
-                type: "AC_POWER_SOURCE_modular_V1.0",
-                slot: 1,
+                type: "AC_POWER_SOURCE_modular_V1_0",
+                slot: parseInt(node.slot),
                 method: "set_current",
-                current_value: parseFloat(node.current_value) 
+                current_value: parseFloat(node.current_value),
+                get_output: {},
+                compare: {}
             }
             var file = globalContext.get("exportFile")
             var slot = globalContext.get("slot");
             if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
             else{file.slots[slot].jig_error.push(command)}
             globalContext.set("exportFile", file);
-            // node.status({fill:"green", shape:"dot", text:"done"}); 
-            msg.payload = command
+            console.log(command)
             send(msg)
         });
             
