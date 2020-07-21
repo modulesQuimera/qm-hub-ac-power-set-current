5f800f2a5ea7939f36a66170fdbe3758a9dcfadf
STRNEOH <strneoh@localhost.localdomain>
2020-07-21 15:27:24 -0300

    adding description

--- set_current.js
+++ set_current.js
@@ -2,9 +2,9 @@ module.exports = function(RED) {
 
     function SetCurrentNode(config) {
         RED.nodes.createNode(this, config);
-        this.current_value = config.current_value
-        this.slot = config.slot
-        var node = this
+        this.current_value = config.current_value;
+        this.slot = config.slot;
+        var node = this;
 
         node.on('input', function(msg, send, done) {
             var globalContext = node.context().global;
@@ -17,16 +17,31 @@ module.exports = function(RED) {
                 current_value: parseFloat(node.current_value),
                 get_output: {},
                 compare: {}
-            }
-            var file = globalContext.get("exportFile")
+            };
+            var file = globalContext.get("exportFile");
             var slot = globalContext.get("slot");
-            if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
-            else{file.slots[slot].jig_error.push(command)}
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command);
+                    // file.begin.push(command);
+                }
+                else{
+                    file.slots[3].jig_test.push(command);
+                    // file.end.push(command);
+                }
+            }
             globalContext.set("exportFile", file);
-            console.log(command)
-            send(msg)
-        });
-            
+            console.log(command);
+            send(msg);
+        });    
     }
     RED.nodes.registerType("set_current", SetCurrentNode);
-}
+};
